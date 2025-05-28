9508b9db0cc1acc0a67d6b92674fee548139352a
shimonkh <shimonkh@passportcard.com>
2025-05-28 15:33:33 +0300

    Fix healthcheck issues

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`✅ App is listening on http://localhost:${port}`);
+  setInterval(() => {
+    console.log('✅ Still alive...');
+  }, 1500);
 
 }
 bootstrap();
